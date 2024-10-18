a9e0d6413261d5119b5aab1145b0504471e6995d
DinoSage <dinosage5632@gmail.com>
2024-10-17 20:03:38 -0400

    Finished First touch_ball_nophysics env

--- gymnasium_env/envs/touch_ball_nophysics.py
+++ gymnasium_env/envs/touch_ball_nophysics.py
@@ -2,6 +2,7 @@ import numpy as np
 from enum import Enum
 
 import gymnasium as gym
+import math
 from gymnasium import spaces
 
 class Actions(Enum):
@@ -14,6 +15,9 @@ class TouchBallNoPhysicsEnv(gym.Env):
     def __init__(self, field_width=304.8, field_height=426.72):
         self.field_width = field_width
         self.field_height = field_height
+        self.turn_speed = 5
+        self.move_speed = 5
+        self.close_enough_radius = 3
 
         # Observations are dictionaries with the agent's and the target's location.
         # Each location is encoded as an element of {0, ..., `size`}^2, i.e. MultiDiscrete([size, size]).
@@ -51,9 +55,44 @@ class TouchBallNoPhysicsEnv(gym.Env):
         # We will sample the target's location randomly until it does not coincide with the agent's location
         self._ball_location = np.array([self.np_random.uniform(0, self.field_width)], [self.np_random.uniform(0, self.field_height)])
 
+        # Random agent orientation
+        self._agent_angle = np.array([self.np_random.uniform(0, 360)])
+
         observation = self._get_obs()
         info = self._get_info()
 
         return observation, info
 
+    def step(self, action):
+
+        # Perform action
+        if (action == Actions.TURN_L):
+            self._agent_angle -= self.turn_speed            
+
+        elif (action == Actions.TURN_R):
+            self._agent_angle += self.turn_speed
+
+        elif (action == Actions.FORWARD):
+            delta_x = self.move_speed * math.cos(math.radians(self._agent_angle))
+            delta_y = self.move_speed * math.sin(math.radians(self._agent_angle))
+            self._agent_location += np.array([delta_x, delta_y])
+            self._agent_location[0] = np.clip(self._agent_location[0], 0, self.field_width) 
+            self._agent_location[1] = np.clip(self._agent_location[1], 0, self.field_height)
+
+
+        # Check if Terminated
+        dist = np.linalg.norm(self._agent_location - self._ball_location)
+        terminated = dist <= self.close_enough_radius
+
+        # Rewards
+
+        reward = 0
+        if (terminated):
+            reward += 1000
+
+
+        observation = self._get_obs()
+        info = self._get_info()
+        
+        return observation, reward, terminated, False, info
 
