5c0ce1395d4aec8b40288d8ee073b9395f48fbf3
Devyansh Jhunjhunwala <djhunjh@purdue.edu>
2024-10-24 19:25:36 -0400

    it works somewhat

--- Env_Test_Copier/gymnasium_env/__init__.py
+++ Env_Test_Copier/gymnasium_env/__init__.py
@@ -3,4 +3,7 @@ from gymnasium.envs.registration import register
 register(
     id="gymnasium_env/TouchBall-v0",
     entry_point="gymnasium_env.envs:TouchBallNoPhysicsEnv",
+    max_episode_steps=300,
+    reward_threshold=None,
+    kwargs = {"render_mode":"human"}
 )
--- test.py
+++ test.py
@@ -3,17 +3,14 @@ import gymnasium_env
 
 from stable_baselines3 import PPO, A2C
 
-models_dir = r"C:\Users\devya\Desktop\Devyansh\Purdue\Clubs\ARC\rl_cartpole_test\models"
-TIMESTEPS = 10_000
+model_path = r"C:\Users\devya\Desktop\Devyansh\Purdue\Clubs\ARC\rl_cartpole_test\models\10000.zip"
 
 env = gym.make("gymnasium_env/TouchBall-v0", render_mode="rgb_array")
 
-model = A2C("MultiInputPolicy", env, verbose=1, device="cuda")
-model.learn(total_timesteps=TIMESTEPS)
-
+model = PPO.load(model_path, env=env)
 vec_env = model.get_env()
 obs = vec_env.reset()
-for i in range(1000):
+for i in range(10_000):
     action, _state = model.predict(obs, deterministic=True)
     obs, reward, done, info = vec_env.step(action)
     vec_env.render("human")
@@ -21,5 +18,4 @@ for i in range(1000):
     if done:
       obs = vec_env.reset()
 
-model.save(f"{models_dir}/{TIMESTEPS}")
 vec_env.close()
